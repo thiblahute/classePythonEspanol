4d8a78a677a6f4c3aa96bdbd6aec0d04f452e46f
Thibault Saunier <tsaunier@src.gnome.org>
2010-04-19 00:58:45 -0400

    Aggrega una demostracion de modulos

---
+++ ejemplo_modulo/demo/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+def testPrint():
+    print "Escribiando desde el modulo demo: fonccion testPrint"
---
+++ ejemplo_modulo/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from demo import test
+
+if __name__ == "__main__":
+    test.testPrint()
