e68863e533c52a15b0e970264df4cc6b4a53ee58
Thibault Saunier <tsaunier@src.gnome.org>
2010-04-25 13:59:15 -0400

    Agregar explicacion de la interfaz de Glade

--- ejemplo_glade/ejemplo.py
+++ ejemplo_glade/ejemplo.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import gtk
-import sys
 
 class ejemplo:
     def on_window_destroy(self, widget, data=None):
@@ -9,11 +8,11 @@ class ejemplo:
 
     def __init__(self):
         consturctor_interfaz = gtk.Builder() #Instanciamos un objeto para constuir la interfaz
-        builder.add_from_file ("ejemplo.glade") #Indicamos al "Builder" cual es el archivo que describe la interfaz
-        self.ventana = builder.get_object("window") #Guardamos el objeto que describe la ventana en un objeto de classe 
-        builder.connect_signals(self)
+        consturctor_interfaz.add_from_file ("ejemplo.glade") #Indicamos al "Builder" cual es el archivo que describe la interfaz
+        self.ventana = consturctor_interfaz.get_object("window") #Guardamos el objeto que describe la ventana en un objeto de classe 
+        consturctor_interfaz.connect_signals(self)
 
 if __name__ == "__main__":
     ejemplo = ejemplo() #Instaciamos la classe "ejemplo", (eso llama el metodo "__init__")
-    ejemplo.window.show() #Mostramos la ventana
+    ejemplo.ventana.show() #Mostramos la ventana
     gtk.main()
